988090d8ca5f817cdc7be9ed78c280598d09b64d
huitquatredev <julien@huitquatre.fr>
2025-04-28 11:32:38 +0200

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,6 +48,7 @@ export default defineConfig({
         faq: "storyblok/Faq",
         timeline: "storyblok/Timeline",
         testimonialStats: "storyblok/TestimonialStats",
+        logoGallery: "storyblok/LogoGallery",
       },
     }),
     sitemap({
