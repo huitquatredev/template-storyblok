914dfbda73dbecbf781f1b62e35e660b0fb28c13
huitquatredev <julien@huitquatre.fr>
2025-05-06 16:16:43 +0200

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import StoryblokClient from "storyblok-js-client";
 import { resolve } from "node:path";
-import { shield } from "@kindspells/astro-shield";
 
 const rootDir = new URL(".", import.meta.url).pathname;
 const modulePath = resolve(rootDir, "src", "generated", "sriHashes.mjs");
@@ -70,29 +69,6 @@ export default defineConfig({
     }),
     sitemap({
       filter: (page) => !filteredUrls.includes(page),
-    }),
-    shield({
-      // - If set, it controls how the security headers will be generated.
-      // - If not set, no security headers will be generated.
-      securityHeaders: {
-        // This option is required to configure CSP headers for your static
-        // content on Netlify.
-        enableOnStaticPages: { provider: "netlify" },
-
-        // - If set, it controls how the CSP (Content Security Policy) header
-        //   will be generated.
-        // - If not set, no CSP header will be configured for your static
-        //   content (there is no need to specify its inner options).
-        contentSecurityPolicy: {
-          // - If set, it controls the "default" CSP directives (they can be
-          //   overriden at runtime).
-          // - If not set, Astro-Shield will use a minimal set of default
-          //   directives.
-          cspDirectives: {
-            "default-src": "'self'",
-          },
-        },
-      },
     }),
   ],
   site: siteurl,
