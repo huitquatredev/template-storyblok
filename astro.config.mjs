a563227913e18f93de89e2871ff29eff36d14e05
huitquatredev <julien@huitquatre.fr>
2025-05-19 21:11:54 +0200

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,7 @@ import devtoolsJson from "vite-plugin-devtools-json";
 
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 //TODO : change site url
-const siteurl = "https://demo-huitquatre.netlify.app";
+const siteurl = "https://demo-preview-huitquatre.netlify.app";
 
 // 1. Initialize the Client with the token
 const Storyblok = new StoryblokClient({
